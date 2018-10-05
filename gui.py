263bdc6d448d00c4559df3a92b7b44b2f38a5ab1
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2018-10-05 11:16:11 +0200

    Création d'une fenêtre et bases des fonctions draw

--- gui.py
+++ gui.py
@@ -1,17 +1,33 @@
-#Importation des bibliothèques nécessaires
+
+#Import library
 import pygame
+import time
+
 from pygame.locals import *
 
-#Initialisation de la bibliothèque Pygame
-pygame.init()
+class GameWindow :
 
-#Création de la fenêtre
-fenetre = pygame.display.set_mode((640, 480))
+	def __init__(self):
+		pygame.init() #Initialization of the library
 
-#Variable qui continue la boucle si = 1, stoppe si = 0
-continuer = 1
+		self.screen = pygame.display.set_mode((640, 480)) #Creation of the window object
 
-#Boucle infinie
-while continuer:
-	continue #Je place continue ici pour pouvoir relancer la boucle infinie
-                 #mais il est d'habitude remplacé par une suite d'instructions
+		start = time.time()
+
+		fond = pygame.image.load("graphics/backgroundTest.jpg").convert()
+		self.screen.blit(fond, (0, 0))
+		self.screen.fill([10, 10, 70])
+
+		#pygame.draw.ellipse(screen, RED, [225, 10, 50, 20], 2)
+
+		pygame.display.flip()
+
+
+		end = time.time()
+		# Boucle infinie
+		while end-start < 2:
+			end = time.time()
+
+window1 = GameWindow()
+
+
