0e5f46e93bc865bfa8ea04e3e6f172f1b6983d48
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2018-10-05 11:53:56 +0200

    Comments and code separation

--- gui.py
+++ gui.py
@@ -12,27 +12,37 @@ class GameWindow:
 
         self.screen = pygame.display.set_mode((640, 480))  # Creation of the window object
 
-        start = time.time()
+        start = time.time() #Time measurement
 
         fond = pygame.image.load("graphics/backgroundTest.jpg").convert()
-        self.screen.blit(fond, (0, 0))
-        self.screen.fill([10, 10, 70])
+        self.screen.blit(fond, (0, 0)) #Draw an image at the position 0,0
+        self.screen.fill([10, 10, 70]) #Fill the screen (background color)
 
-        self.gridWidth = 300
-        self.cellSize = self.gridWidth/3
-        self.gridPos = [10,10]
-        self.cellPos = [self.gridPos[0]+int(self.cellSize/2),self.gridPos[1]+int(self.cellSize/2)]
+        self.gridWidth = 300 #Overall width of the grid (can be modified)
+        self.cellSize = self.gridWidth/3 #Size of a cell depending on the grid width
+        self.gridPos = [10,10] #Position of the top left corner of the grid
+        self.cellPos = [self.gridPos[0]+int(self.cellSize/2),self.gridPos[1]+int(self.cellSize/2)] #Position of the center of the top left circle
+
+        self.drawGrid()
 
         pygame.draw.circle(self.screen, [10,200,200], self.cellPos,25,2)
-        pygame.draw.rect(self.screen, [255,255,255], self.gridPos+[self.gridWidth,self.gridWidth],2)
-        pygame.draw.rect(self.screen, [255, 255, 255],[self.gridPos[0], self.gridPos[1]+self.cellSize] + [self.gridWidth, self.cellSize], 2)
-        pygame.draw.rect(self.screen, [255, 255, 255],[self.gridPos[0]+ self.cellSize, self.gridPos[1]] + [self.cellSize,self.gridWidth], 2)
         pygame.display.flip()
 
         end = time.time()
         # Boucle infinie
         while end - start < 2:
             end = time.time()
+
+    def drawGrid(self):
+        """Draw all the edges of the morpion grid taking into account the grid position and its size"""
+        pygame.draw.rect(self.screen, [255, 255, 255], self.gridPos + [self.gridWidth, self.gridWidth], 2)
+        pygame.draw.rect(self.screen, [255, 255, 255],
+                         [self.gridPos[0], self.gridPos[1] + self.cellSize] + [self.gridWidth, self.cellSize], 2)
+        pygame.draw.rect(self.screen, [255, 255, 255],
+                         [self.gridPos[0] + self.cellSize, self.gridPos[1]] + [self.cellSize, self.gridWidth], 2)
+
+    def drawCurrentState(self, matrix):
+        """Draw the current state of the grid, (all the circles) taking into account an input state matrix"""
 
 
 window1 = GameWindow()
