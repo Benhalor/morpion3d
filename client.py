5318b7d6ba857f3652cf11c594029db7a1f4e50c
Gabriel <gabriel.moneyron@yahoo.fr>
2018-10-05 12:02:33 +0200

    implement client server connection

--- client.py
+++ client.py
@@ -8,62 +8,60 @@ Created on Fri Oct  5 10:09:31 2018
 
 import socket
 import numpy as np
+import time
 
 class Client():
-    def __init__(self, name):
+    def __init__(self, name, address, port):
         self.name = name
+        self.address = address
+        self.port = port
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.matrixSize = (3,3,3)
+        self.playerId = None
 
     def connect(self):
-        self.connection.connect(('localhost', 12800))
+        self.connection.connect((self.address, self.port))
         print("Client " + self.name + " is joining server...")
         self.connection.send(b"JOIN")
         received_message = "DENIED"
-        while(received_message != "ACCEPTED"):
+        while(received_message == "DENIED"):
             try:
                 received_message = self.connection.recv(1024).decode()
+                self.playerId = int(received_message)
             except Exception as e:
                 print(e)
-        print("Client " + self.name + " is connected to server.")
+        print("Client " + self.name + " is connected to server with ID : "+str(self.playerId))
 
     def send_matrix(self, matrix):
         matrix = np.array(matrix)
         shape = matrix.shape
         if shape == self.matrixSize:
             command = "MATRIX/"+str(self.matrixSize[0])+"/"+str(self.matrixSize[1])+"/"+str(self.matrixSize[2])+"/"
-            print(command.encode()+b"0")
+            command = command.encode()
+
+            for i in range(self.matrixSize[0]):
+                for j in range(self.matrixSize[1]):
+                    for k in range(self.matrixSize[2]):
+                        command += str(matrix[i][j][k]).encode()
+            print(command)
         else:
             raise Exception("Matrix size is wrong")
 
     def close_client(self):
         self.connexion_avec_serveur.send(b"QUIT")
 
-    def run(self):
+    def play(self):
+        if self.playerId == 0:
 
-        msg_recu = self.connection.recv(1024)
-        if msg_recu == b"HLO":
-            self.connection.send(b"HLO")
-        else:
-            raise Exception("Messge recu non valide : " + msg_recu.decode())
-        command = input("Entrez une commande: ").replace("\n", "").split(" ")
-        while "QUT" not in command:
-            if len(command)==3:
-                operateur = command[0].encode()
-                nb1 = float(command[1])
-                nb2 = float(command[2])
-                nb1_byte = bytearray(struct.pack("d", nb1))
-                nb2_byte = bytearray(struct.pack("d", nb2))
-                self.connexion_avec_serveur.send(operateur + nb1_byte + nb2_byte)
-                msg_recu = self.connexion_avec_serveur.recv(1024).decode()
-                print("Valeur renvoyée: "+msg_recu)
-            else:
-                print("Mauvaise syntaxe. Exemple : ADD 1.23 55.3")
-            command = input("Entrez une commande: ").replace("\n", "").split(" ")
-        self.connexion_avec_serveur.send(b"QUT")
 
-        print("Client fermé")
+
 
 if __name__ == '__main__':
-    client = Client("Gabriel")
-    client.send_matrix([[[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]], [[0,0,0],[0,0,0],[0,0,0]]])
+    gabriel = Client("Gabriel", 'localhost', 12800)
+    gabriel.connect()
+
+    sylvestre = Client("Sylvestre", 'localhost', 12800)
+    sylvestre.connect()
+
+    time.sleep(1)
+    gabriel.send_matrix([[[0,0,0], [0,0,0], [0,0,0]], [[0,0,0], [0,0,0], [0,0,0]], [[0,0,0], [0,0,0], [0,0,0]]])
