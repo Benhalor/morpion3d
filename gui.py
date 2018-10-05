61c20fc895da2f426d0c73d9fa0a787dd4303d13
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2018-10-05 11:40:54 +0200

    Grid basic design

--- gui.py
+++ gui.py
@@ -1,33 +1,38 @@
-
-#Import library
+# Import library
 import pygame
 import time
 
 from pygame.locals import *
 
-class GameWindow :
 
-	def __init__(self):
-		pygame.init() #Initialization of the library
+class GameWindow:
 
-		self.screen = pygame.display.set_mode((640, 480)) #Creation of the window object
+    def __init__(self):
+        pygame.init()  # Initialization of the library
 
-		start = time.time()
+        self.screen = pygame.display.set_mode((640, 480))  # Creation of the window object
 
-		fond = pygame.image.load("graphics/backgroundTest.jpg").convert()
-		self.screen.blit(fond, (0, 0))
-		self.screen.fill([10, 10, 70])
+        start = time.time()
 
-		#pygame.draw.ellipse(screen, RED, [225, 10, 50, 20], 2)
+        fond = pygame.image.load("graphics/backgroundTest.jpg").convert()
+        self.screen.blit(fond, (0, 0))
+        self.screen.fill([10, 10, 70])
 
-		pygame.display.flip()
+        self.gridWidth = 300
+        self.cellSize = self.gridWidth/3
+        self.gridPos = [10,10]
+        self.cellPos = [self.gridPos[0]+int(self.cellSize/2),self.gridPos[1]+int(self.cellSize/2)]
 
+        pygame.draw.circle(self.screen, [10,200,200], self.cellPos,25,2)
+        pygame.draw.rect(self.screen, [255,255,255], self.gridPos+[self.gridWidth,self.gridWidth],2)
+        pygame.draw.rect(self.screen, [255, 255, 255],[self.gridPos[0], self.gridPos[1]+self.cellSize] + [self.gridWidth, self.cellSize], 2)
+        pygame.draw.rect(self.screen, [255, 255, 255],[self.gridPos[0]+ self.cellSize, self.gridPos[1]] + [self.cellSize,self.gridWidth], 2)
+        pygame.display.flip()
 
-		end = time.time()
-		# Boucle infinie
-		while end-start < 2:
-			end = time.time()
+        end = time.time()
+        # Boucle infinie
+        while end - start < 2:
+            end = time.time()
+
 
 window1 = GameWindow()
-
-
