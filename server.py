3bb8b73ee70f211004b38159c58eb4625257c7e9
Gabriel <gabriel.moneyron@yahoo.fr>
2018-10-08 22:22:26 +0200

    add 2 dimensional and 3d support add communication between server nd client and matrix reading and sending

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ Created on Fri Oct  5 10:09:31 2018
 
 #python PycharmProjects/morpion3d/server.py
 import socket
+import numpy as np
 from threading import Thread
 
 class Server(Thread):
@@ -18,6 +19,8 @@ class Server(Thread):
         self.listOfConnections = []
         self.listOfInfoConnections = []
         self.idCounter = 0
+        self.matrixSize = None
+        self.matrix = []
         print("Server started")
 
     def connect_clients (self):
@@ -30,6 +33,18 @@ class Server(Thread):
         for i in range(self.numberOfPlayers):
             tempConnection , tempsInfoConnection = self.main_connection.accept()
             tempConnection.send(str(self.idCounter).encode())
+            received_message = tempConnection.recv(1024).decode()
+
+            #read dimension of matrix and check that all players want the same
+            if self.matrixSize == None:
+                self.matrixSize = [int(element) for element in received_message.split("/")]
+                self.matrix = np.zeros(self.matrixSize)
+            else:
+                if self.matrixSize != [int(element) for element in received_message.split("/")]:
+                    raise Exception("Players doesnt want the same dimension...")
+                else:
+                    print("Dimension checked: " + str(self.matrixSize))
+
             print((bytearray(self.idCounter)))
             self.idCounter+=1
             self.listOfConnections.append(tempConnection)
@@ -37,15 +52,68 @@ class Server(Thread):
             print(tempsInfoConnection)
         print("Clients connected")
 
+    def read_matrix(self, string):
+        print(string)
+        matrix = np.zeros(self.matrixSize)
+        dimension = len(self.matrixSize)
+        if  dimension == 2:
+            count = 0
+            for i in range(self.matrixSize[0]):
+                for j in range(self.matrixSize[1]):
+                    matrix[i,j] = int(string[count])
+                    count += 1
+
+        elif dimension == 3:
+            count = 0
+            for i in range(self.matrixSize[0]):
+                for j in range(self.matrixSize[1]):
+                    for k in range(self.matrixSize[2]):
+                        matrix[i,j,k] = int(string[count])
+                        count +=1
+        else:
+            raise Exception("Dimension is not supported")
+        print(matrix)
+        return matrix
+
+    def send_matrix(self,connection, matrix):
+        print("Sending matrix to client")
+
+        command = "MATRIX/"
+        command = command.encode()
+
+        dimension = len(self.matrixSize)
+        if dimension == 2:
+            for i in range(self.matrixSize[0]):
+                for j in range(self.matrixSize[1]):
+                    command += str(int(matrix[i][j])).encode()
+
+        elif dimension == 3:
+            for i in range(self.matrixSize[0]):
+                for j in range(self.matrixSize[1]):
+                    for k in range(self.matrixSize[2]):
+                        command += str(int(matrix[i][j][k])).encode()
+        else:
+            raise Exception("Dimension is not supported")
+        print(command)
+        connection.send(command)
+
+
     def run(self):
         received_message = "OK"
         while (True):
             for i in range (self.numberOfPlayers):
-                self.listOfConnections[i].send(b"YOURTURN")
+                self.send_matrix(self.listOfConnections[i], self.matrix)
                 received_message = self.listOfConnections[i].recv(1024).decode()
+                print("SERVER: "+received_message)
+                if "MATRIX" in received_message:
+                    self.matrix = self.read_matrix(received_message.split("/")[-1])
+                    self.send_matrix(self.listOfConnections[i], self.matrix)
         print("run")
+
+
 
 
 if __name__ == '__main__':
     server = Server(12800)
     server.connect_clients()
+    server.start()
