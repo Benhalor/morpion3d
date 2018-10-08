1c33f3ba25373614ae5c316fbe8a43422f16e734
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2018-10-08 16:22:09 +0200

    File containing the main window class

---
+++ guiMainWindow.py
@@ -0,0 +1,18 @@
+from __future__ import division
+
+import pygame
+from guiGameWindow2D import GameWindow2D
+
+
+class MainWindow:
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((640, 480))
+        self.gui2D = GameWindow2D(self.screen,300, [100, 100])
+
+
+
+
+gui = MainWindow()
+
