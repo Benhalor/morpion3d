b4e94923ec08e2cc58e59675fcc329d8d7b7509e
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2018-10-09 14:54:02 +0200

    Class MainWindow containing 2 methods : get_played_cell and send_state_matrix

--- guiMainWindow.py
+++ guiMainWindow.py
@@ -11,5 +11,10 @@ class MainWindow:
         self.screen = pygame.display.set_mode((640, 480))
         self.gui2D = GameWindow2D(self.screen, 300, [100, 100])
 
+    def get_played_cell(self):
+        return self.gui2D.get_played_cell()
+
+    def send_state_matrix(self, matrix):
+        self.gui2D._set_state_matrix(matrix)
 
 gui = MainWindow()
