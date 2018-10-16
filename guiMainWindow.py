99665f2312a93c7a9116bcd0186c52a6143b0a00
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2018-10-16 15:09:35 +0200

    Moved some codes from run to __init__ method.

--- guiMainWindow.py
+++ guiMainWindow.py
@@ -15,10 +15,6 @@ class MainWindow(Thread):
         self.dim3Dor2D = dim3Dor2D
         self.dim = dim
 
-    def run(self):
-
-        # The following part should not be in the init. Otherwise, the event handler is in the wrong thread
-        pygame.init()
         self.screen = pygame.display.set_mode((640, 480))
         if self.dim3Dor2D == 2:
             self.gui = GameWindow2D(self, 300, [100, 100], self.dim)
@@ -27,8 +23,11 @@ class MainWindow(Thread):
 
         self.textMessage = "Bonjour le monde. Ceci est un test."
 
-        self.update_screen()
+    def run(self):
 
+        # The following part should not be in the init. Otherwise, the event handler is in the wrong thread
+        pygame.init()
+        self.update_screen()
 
         boolContinue = True
         # Event management
