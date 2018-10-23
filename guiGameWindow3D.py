fa71efd3b3551694d4845a328901b8ca448ac4e1
AB036 <armand.bouvier-neveu@student.ecp.fr>
2018-10-23 18:33:12 +0200

    some changes

--- guiGameWindow3D.py
+++ guiGameWindow3D.py
@@ -1,79 +1,51 @@
-import pygame
-
+#import numpy as np
 from guiDrawer import Drawer
 from perspectiveprojection import *
 
 
 class GameWindow3D:
 
-    def __init__(self, parentWindow, gridWidth, gridPos, dim):
+    def __init__(self, parentWindow, gridWidth, gridSize):
         """Take as input the size of the grid and the position of the top left corner of the grid"""
         self.parentWindow = parentWindow
         self.screen = parentWindow.get_screen()
         self.drawer = Drawer(self.screen)
-
-
-
-
-        self._gridDim = dim  # Dimension of the grid (default = 3)
+        
+        self._gridSize = gridSize  # Size of the grid (default = 3)
         self._gridWidth = gridWidth  # gridWidth  # Overall width of the grid (real 3d width)
-        self._cellSize = self.gridWidth / self.gridDim
-        self._gridPos = np.array(gridPos)  # Position in the screen of the top left corner of the grid (highest point in
-        #  the screen in isometric view)
+        self._cellSize = gridWidth / gridSize
+        self._heightSeparation = 22 / self.gridSize  # Distance between each plane in the screen
 
-        self.heightSeparation = 22 / self.gridDim  # Distance between each plane in the screen
-
-        self._stateMatrix = np.zeros([self.gridDim, self.gridDim, self.gridDim])
-        self._coloringMatrix = np.zeros([self.gridDim, self.gridDim, self.gridDim])
+        self._stateMatrix = np.zeros([self.gridSize, self.gridSize, self.gridSize])
+        self._coloringMatrix = np.zeros([self.gridSize, self.gridSize, self.gridSize])
 
         self.selectedCell = [-1, -1, -1]  # Coordinates of the selected cell ([-1,-1,-1] if no cell is selected)
 
-        self.space = Space()
-        axx, axy, axz = self.space.axes
-        # print(self.space.axes)
-        # self.space.axes = (axx,axy,(-axz[0],-axz[1],-axz[2]))
-
-        self.points = []
-        self.polygons = []
-
+        self._space = Space()        
+        
+        self._points = [[[ None for k in range(self._gridSize+1)]for j in range(self._gridSize+1)] for i in range(self._gridSize+1)]
+        
+        for i in range(self._gridSize+1):
+            for j in range(self._gridSize+1):
+                for k in range(self._gridSize+1):
+                    xp = -self.gridWidth / 2 + i * self._cellSize
+                    yp = -self.gridWidth / 2 + j * self._cellSize
+                    zp = (k - (self.gridSize - 1) // 2) * self._heightSeparation
+                    self.points[i][j][k] = Point(self._space, xp, yp, zp)
+        
+        self._polygons = [[[ None for k in range(self.gridSize)]for j in range(self.gridSize)] for i in range(self.gridSize)]
+        
+        for i in range(self._gridSize):
+            for j in range(self._gridSize):
+                for k in range(self._gridSize):
+                    P1 = self._points[i][j][k]
+                    P2 = self._points[i+1][j][k]
+                    P3 = self._points[i][j+1][k]
+                    P4 = self._points[i+1][j+1][k]
+                    self._polygons[i][j][k] = Polygon(self._space, [P1,P2,P4,P3], name = 'cell' + str(i) + str(j) + str(k))
+        
         self.statePoints1 = []
         self.statePoints2 = []
-
-        for i in range(self.gridDim):
-            self.points.append([])
-            self.polygons.append([])
-            for j in range(self.gridDim):
-                self.points[i].append([])
-                self.polygons[i].append([])
-                for k in range(self.gridDim):
-                    self.points[i][j].append([])
-
-                    if i == 0 or j == 0:
-                        self.points[i][j][k].append(Point(self.space, -self.gridWidth / 2 + i * self._cellSize,
-                                                          -self.gridWidth / 2 + j * self._cellSize,
-                                                          (k - int((self.gridDim - 1) / 2)) * self.heightSeparation))
-                    else:
-                        self.points[i][j][k].append(self.points[i - 1][j - 1][k][2])
-
-                    if j == 0:
-                        self.points[i][j][k].append(Point(self.space, -self.gridWidth / 2 + (i + 1) * self._cellSize,
-                                                          -self.gridWidth / 2 + j * self._cellSize,
-                                                          (k - int((self.gridDim - 1) / 2)) * self.heightSeparation))
-                    else:
-                        self.points[i][j][k].append(self.points[i][j - 1][k][2])
-
-                    self.points[i][j][k].append(Point(self.space, -self.gridWidth / 2 + (i + 1) * self._cellSize,
-                                                      -self.gridWidth / 2 + (j + 1) * self._cellSize,
-                                                      (k - int((self.gridDim - 1) / 2)) * self.heightSeparation))
-
-                    if i == 0:
-                        self.points[i][j][k].append(Point(self.space, -self.gridWidth / 2 + i * self._cellSize,
-                                                          -self.gridWidth / 2 + (j + 1) * self._cellSize,
-                                                          (k - int((self.gridDim - 1) / 2)) * self.heightSeparation))
-                    else:
-                        self.points[i][j][k].append(self.points[i - 1][j][k][2])
-                    self.polygons[i][j].append(
-                        Polygon(self.space, [self.points[i][j][k][p] for p in range(4)], str(i) + str(j) + str(k)))
 
         self.statePoints1.append(Point(self.space, 0, 0, 0))
         self.statePoints1.append(Point(self.space, -self._cellSize / 2.5, -self._cellSize / 2.5, 0))
@@ -139,9 +111,9 @@ class GameWindow3D:
         """Draw all the edges of the morpion grid taking into account the grid position and its size"""
 
         self.drawer.erase()
-        for k in range(self.gridDim - 1, -1, -1):
-            for i in range(self.gridDim):
-                for j in range(self.gridDim):
+        for k in range(self.gridSize - 1, -1, -1):
+            for i in range(self.gridSize):
+                for j in range(self.gridSize):
                     if self.selectedCell == [i, j, k]:
                         self.drawer.drawCell(self.polygons[i][j][k],1) # 1 = Selected cell
                     else :
@@ -149,7 +121,7 @@ class GameWindow3D:
                     if self.stateMatrix[i, j, k] != 0:
                         translation = [-self.gridWidth / 2 + (i + 1 / 2) * self._cellSize,
                                        -self.gridWidth / 2 + (j + 1 / 2) * self._cellSize,
-                                       (k - int((self.gridDim - 1) / 2)) * self.heightSeparation]
+                                       (k - int((self.gridSize - 1) / 2)) * self.heightSeparation]
                         if self.stateMatrix[i, j, k] == 1:
                             self.drawer.drawState(self.statePolygon1,translation,1)
                         elif self.stateMatrix[i, j, k] == 2:
@@ -170,17 +142,11 @@ class GameWindow3D:
     def _set_grid_width(self, newGridWidth):
         self._gridWidth = newGridWidth
 
-    def _get_grid_pos(self):
-        return self._gridPos
+    def _get_grid_size(self):
+        return self._gridSize
 
-    def _set_grid_pos(self, newGridPos):
-        self._gridPos = np.array(newGridPos)
-
-    def _get_grid_dim(self):
-        return self._gridDim
-
-    def _set_grid_dim(self, newGridDim):
-        self._gridDim = list(newGridDim)
+    def _set_grid_size(self, newgridSize):
+        self._gridSize = list(newgridSize)
 
     def _get_state_matrix(self):
         return self._stateMatrix
@@ -191,6 +157,5 @@ class GameWindow3D:
         self.parentWindow.update_screen()
 
     gridWidth = property(_get_grid_width, _set_grid_width)
-    gridPos = property(_get_grid_pos, _set_grid_pos)
-    gridDim = property(_get_grid_dim, _set_grid_dim)
+    gridSize = property(_get_grid_size, _set_grid_size)
     stateMatrix = property(_get_state_matrix, _set_state_matrix)
