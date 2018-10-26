fedc566dad9bef6066a1a7924dbe69812c80043b
AB036 <armand.bouvier-neveu@student.ecp.fr>
2018-10-26 09:53:40 +0200

    hh

--- guiGameWindow3D.py
+++ guiGameWindow3D.py
@@ -8,18 +8,17 @@ class GameWindow3D:
     def __init__(self, parentWindow, gridWidth, gridSize):
         """Take as input the size of the grid and the position of the top left corner of the grid"""
         self._parentWindow = parentWindow
-        self._screen = parentWindow.get_screen()
-        self._drawer = Drawer(self.screen)
+        self._drawer = Drawer(parentWindow.screen)
         
         self._gridSize = gridSize  # Size of the grid (default = 3)
         self._gridWidth = gridWidth  # gridWidth  # Overall width of the grid (real 3d width)
         self._cellSize = gridWidth / gridSize
         self._heightSeparation = 22 / self.gridSize  # Distance between each plane in the screen
 
-        self._stateMatrix = np.zeros([self.gridSize, self.gridSize, self.gridSize])
-        self._coloringMatrix = np.zeros([self.gridSize, self.gridSize, self.gridSize])
+        self._stateMatrix = np.zeros([self._gridSize, self._gridSize, self._gridSize])
+        self._coloringMatrix = np.zeros([self._gridSize, self._gridSize, self._gridSize])
 
-        self._selectedCell = [-1, -1, -1]  # Coordinates of the selected cell ([-1,-1,-1] if no cell is selected)
+        self._selectedCell = (-1, -1, -1)  # Coordinates of the selected cell ([-1,-1,-1] if no cell is selected)
 
         self._space = Space()        
         
@@ -79,48 +78,42 @@ class GameWindow3D:
         self._parentWindow.update_screen()
 
     def detect_cell_pos(self, mousePos):
-        """Changes the cell coordinates corresponding to the mouse position ([-1,-1] = out of the grid)"""
-        cellPos = [-1, -1, -1]
-        ((xmin, ymin), (xmax, ymax)) = self.space.xyBounds
-        if (xmin <= mousePos[0] <= xmax) and (ymin < mousePos[1] < ymax) :
-            detectedPolygon = self.space.locate_polygon(mousePos[0], mousePos[1])
-            if detectedPolygon != None:
-                cellPos = [int(detectedPolygon.name[0]), int(detectedPolygon.name[1]), int(detectedPolygon.name[2])]
-            self.selectedCell = cellPos
+        """Changes the cell coordinates corresponding to the mouse position ([-1,-1,-1] = out of the grid)"""
+        cellPos = (-1, -1, -1)
+        ((xmin, ymin), (xmax, ymax)) = self._space.xyBounds
+        if (xmin <= mousePos[0] <= xmax) and (ymin <= mousePos[1] <= ymax) :
+            detectedPolygon = self._space.locate_polygon(mousePos[0], mousePos[1])
+            if detectedPolygon is not None:
+                cellPos = (int(detectedPolygon.name[4]), int(detectedPolygon.name[5]), int(detectedPolygon.name[6]))
+            self._selectedCell = cellPos
 
-            self.parentWindow.textMessage = str(self.selectedCell[0]) + str(self.selectedCell[1]) + str(
-                self.selectedCell[2]) \
-                                            + " MinDepth=" + str(
-                int(self.polygons[cellPos[0]][cellPos[1]][cellPos[2]].depth[0])) \
-                                            + " AvgDepth=" + str(
-                int(self.polygons[cellPos[0]][cellPos[1]][cellPos[2]].depth[1])) \
-                                            + " MaxDepth=" + str(
-                int(self.polygons[cellPos[0]][cellPos[1]][cellPos[2]].depth[2]))
+            #self.parentWindow.textMessage = str(self.selectedCell[0]) + str(self.selectedCell[1]) + str(self.selectedCell[2])
 
     # ================ DRAWING METHODS ======================================
 
     def draw_grid(self):
         """Draw all the edges of the morpion grid taking into account the grid position and its size"""
 
-        self.drawer.erase()
-        for k in range(self.gridSize - 1, -1, -1):
-            for i in range(self.gridSize):
-                for j in range(self.gridSize):
-                    if self.selectedCell == [i, j, k]:
-                        self.drawer.drawCell(self.polygons[i][j][k],1) # 1 = Selected cell
-                    else :
-                        self.drawer.drawCell(self.polygons[i][j][k], 1)  # 0 = Unselected cell
-                    if self.stateMatrix[i, j, k] != 0:
-                        translation = [-self.gridWidth / 2 + (i + 1 / 2) * self._cellSize,
+        self._drawer.erase()
+        for poly in self._space.polygons:
+            name = poly.name
+            if len(name) >= 4 and name[:4] == "cell":
+                i = int(name[4])
+                j = int(name[5])
+                k = int(name[6])
+                self._drawer.draw_cell(poly, 1 if (i,j,k) == self._selectedCell else 0)
+                if self._stateMatrix[i,j,k] != 0:
+                    translation = (-self.gridWidth / 2 + (i + 1 / 2) * self._cellSize,
                                        -self.gridWidth / 2 + (j + 1 / 2) * self._cellSize,
-                                       (k - int((self.gridSize - 1) / 2)) * self.heightSeparation]
-                        if self.stateMatrix[i, j, k] == 1:
-                            self.drawer.drawState(self.statePolygon1,translation,1)
-                        elif self.stateMatrix[i, j, k] == 2:
-                            self.drawer.drawState(self.statePolygon2, translation, 2)
-        if self.selectedCell != [-1, -1, -1]:
-            self.drawer.highlightCell(self.polygons[self.selectedCell[0]][self.selectedCell[1]][self.selectedCell[2]])
-
+                                       (k - int((self.gridSize - 1) / 2)) * self.heightSeparation)
+                    if self._stateMatrix[i,j,k] == 1:
+                        self._drawer.draw_state(self._circlePolygon, translation, 1)
+                    elif self._stateMatrix[i,j,k] == 2:
+                        self._drawer.draw_state(self._crossPolygon, translation, 2)
+        if self._selectedCell != (-1,-1,-1):
+            i,j,k = self._selectedCell
+            self._drawer.highlight_cell(self._polygons[i][j][k])
+                
     def update_screen(self):
         self.draw_grid()
         # self.draw_selected_cell()
@@ -149,7 +142,11 @@ class GameWindow3D:
         self.parentWindow.update_screen()
         return cell
 
+    def _get_selected_cell(self):
+        return self._selectedCell
+    
     gridWidth = property(_get_grid_width)
     gridSize = property(_get_grid_size)
     stateMatrix = property(_get_state_matrix, _set_state_matrix)
     playedCell = property(_get_played_cell)
+    selectedCell = property(_get_selected_cell)
