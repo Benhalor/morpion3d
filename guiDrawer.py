57a91679311a5bb3b5d72230dcb05ba3002eaec0
SylvestreSakti <sylvestre.prabakaran@gmail.com>
2018-10-26 11:30:05 +0200

    Corrected translation handling (tuple)

--- guiDrawer.py
+++ guiDrawer.py
@@ -8,15 +8,15 @@ import numpy as np
 class Drawer:
 
     def __init__(self, screen):
-        self._screen = screen #Gets the pygame screen to draw on it
+        self._screen = screen  # Gets the pygame screen to draw on it
 
-        self._colorHighlight = [255, 255, 0] #Color of the border of a cell to highlight
-        self._color1 = [255, 0, 0] #Color of the cross of the first player
-        self._color2 = [0, 255, 0] #Color of the circle of the opponent
-        self._colorBackground = [100, 100, 100] #Color of the screen background
-        self._gridColor = [150, 150, 255]  #Color of the lines of the grid
-        self._gridSelectColor = [255, 150, 255] #Color to fill a selected cell
-        self._gridLineColor = [0, 0, 100] #Color to fill the grid
+        self._colorHighlight = [255, 255, 0]  # Color of the border of a cell to highlight
+        self._color1 = [255, 0, 0]  # Color of the cross of the first player
+        self._color2 = [0, 255, 0]  # Color of the circle of the opponent
+        self._colorBackground = [100, 100, 100]  # Color of the screen background
+        self._gridColor = [150, 150, 255]  # Color of the lines of the grid
+        self._gridSelectColor = [255, 150, 255]  # Color to fill a selected cell
+        self._gridLineColor = [0, 0, 100]  # Color to fill the grid
 
     def erase(self):
         self._screen.fill(self._colorBackground)  # Fill the screen (background color)
@@ -24,11 +24,11 @@ class Drawer:
     def draw_cell(self, cellPolygon, stateColor=0):
         """Draws a cell taking the corresponding polygon and chose the color depending on the state"""
         pointsList = cellPolygon.xyProjected
-        if stateColor == 0:
+        if stateColor == 0: #If the cell is unselected
             pygame.draw.polygon(self._screen, self._gridColor, pointsList)
-        else:
+        else: #if the cell is selected
             pygame.draw.polygon(self._screen, self._gridSelectColor, pointsList)
-        pygame.draw.aalines(self._screen, self._gridLineColor, True, pointsList)
+        pygame.draw.aalines(self._screen, self._gridLineColor, True, pointsList) #Draws the lines of the grid
 
     def draw_state(self, statePolygon, translation, stateColor):
         """Draws a cross or a circle corresponding to statePolygon (by translating it from the center using translation)"""
@@ -37,7 +37,7 @@ class Drawer:
             pygame.draw.aalines(self._screen, self._color1, True, statePolygon.xyProjected)
         elif stateColor == 2:
             pygame.draw.aalines(self._screen, self._color2, True, statePolygon.xyProjected)
-        statePolygon.translate(-np.array(translation).tolist())
+        statePolygon.translate((-translation[0],-translation[1],-translation[2]))
 
     def highlight_cell(self, cellPolygon):
         pygame.draw.lines(self._screen, self._colorHighlight, True,
