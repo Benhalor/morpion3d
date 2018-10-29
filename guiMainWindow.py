c683120a1223d775d962fd2b355a856aa11ff083
Gabriel <gabriel.moneyron@yahoo.fr>
2018-10-29 17:40:21 +0100

    work

--- guiMainWindow.py
+++ guiMainWindow.py
@@ -23,6 +23,12 @@ class MainWindow(Thread):
         self._gui = None
         self.__lockEvent = threading.Lock()
 
+        self.__boolMoveLeft = False
+        self.__boolMoveRight = False
+        self.__boolMoveUp = False
+        self.__boolMoveDown = False
+        self.__boolRightClick = False
+
     def run(self):
         self.__lockEvent.acquire()
         pygame.init()
@@ -47,39 +53,67 @@ class MainWindow(Thread):
         move = False
 
         while self._boolContinue:
-            if time.time() - starting >= 0.04 and move :
-                if pygame.mouse.get_pos()[0] < 200:
-                    self._gui.move("left", 0.0004 * (200 - pygame.mouse.get_pos()[0]))
-                elif pygame.mouse.get_pos()[0] > 400:
-                    self._gui.move("right", 0.0004 * (pygame.mouse.get_pos()[0] - 400))
-                starting = time.time()
-            event = pygame.event.wait()
-            if self._wantToPlay:
-                if event.type == KEYDOWN:
-                    if event.key == K_LEFT:
-                        self._gui.move("left",0.1)
-                    elif event.key == K_RIGHT:
-                        self._gui.move("right",0.1)
-                    elif event.key == K_UP:
-                        self._gui.move("up",0.1)
-                    elif event.key == K_DOWN:
-                        self._gui.move("down",0.1)
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:  # If left click
-                        cell = self._gui.playedCell
-                        if cell != [-1, -1]:
-                            self.update_screen()
-                            self._cell = cell
-                            self.__lockEvent.release()
-                    elif event.button == 3 :
-                        move = True
-                if event.type == MOUSEBUTTONUP:
-                    if event.button == 3:  # If right click
-                        move = False
-                if event.type == MOUSEMOTION :
-                    #self.gui.move("left")
-                    self._gui.detect_cell_pos(event.pos)
-                    self.update_screen()
+
+            # Rotate view
+            move = self.__boolMoveLeft or self.__boolMoveRight or self.__boolMoveUp or self.__boolMoveDown \
+                   or self.__boolRightClick
+            if move:
+                if time.time() - starting >= 0.06:
+                    if self.__boolMoveLeft:
+                        self._gui.move("left", 0.1)
+                    elif self.__boolMoveRight:
+                        self._gui.move("right", 0.1)
+                    elif self.__boolMoveUp:
+                        self._gui.move("up", 0.1)
+                    elif self.__boolMoveDown:
+                        self._gui.move("down", 0.1)
+                    elif self.__boolRightClick:
+                        x = pygame.mouse.get_pos()[0]
+                        if x < 200:
+                            self._gui.move("left", 0.001 * (200 - x))
+                        elif x > 400:
+                            self._gui.move("right", 0.001 * (x - 400))
+                    starting = time.time()
+                    event = pygame.event.poll()
+            else:
+                event = pygame.event.wait()
+
+            # rotate
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    self.__boolMoveLeft = True
+                elif event.key == K_RIGHT:
+                    self.__boolMoveRight = True
+                elif event.key == K_UP:
+                    self.__boolMoveUp = True
+                elif event.key == K_DOWN:
+                    self.__boolMoveDown = True
+            if event.type == KEYUP:
+                if event.key == K_LEFT:
+                    self.__boolMoveLeft = False
+                elif event.key == K_RIGHT:
+                    self.__boolMoveRight = False
+                elif event.key == K_UP:
+                    self.__boolMoveUp = False
+                elif event.key == K_DOWN:
+                    self.__boolMoveDown = False
+
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1 and self._wantToPlay:  # If left click, select cell if it is your turn
+                    cell = self._gui.playedCell
+                    if cell != [-1, -1]:
+                        self.update_screen()
+                        self._cell = cell
+                        self.__lockEvent.release()
+                elif event.button == 3 :
+                    self.__boolRightClick = True
+            if event.type == MOUSEBUTTONUP:
+                if event.button == 3:  # If right click
+                    self.__boolRightClick = False
+            if event.type == MOUSEMOTION :
+                #self.gui.move("left")
+                self._gui.detect_cell_pos(event.pos)
+                self.update_screen()
             if event.type == QUIT:
                 self.stop()
 
